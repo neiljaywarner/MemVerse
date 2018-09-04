1741ba4a75af4e134fbc8defdf61f974b5e06830
Neil Jay Warner <njwandroid@gmail.com>
2018-09-03 22:00:29 -0500

    does not compile, halfway

--- app/src/main/java/com/spiritflightapps/memverse/ui/ui/addverse/AddVerseFragment.kt
+++ app/src/main/java/com/spiritflightapps/memverse/ui/ui/addverse/AddVerseFragment.kt
@@ -1,5 +1,6 @@
 package com.spiritflightapps.memverse.ui.ui.addverse
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -7,19 +8,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.crashlytics.android.Crashlytics
 import com.spiritflightapps.memverse.R
+import com.spiritflightapps.memverse.model.VerseResponse
+import com.spiritflightapps.memverse.network.MemverseApi
+import com.spiritflightapps.memverse.network.ServiceGenerator
 import com.spiritflightapps.memverse.utils.Analytics
 import com.spiritflightapps.memverse.utils.BOOKS_OF_BIBLE
 import com.spiritflightapps.memverse.utils.Prefs
 import com.spiritflightapps.memverse.utils.TRANSLATIONS_ABBREVIATIONS_TEXT
 import kotlinx.android.synthetic.main.add_verse_fragment.*
+import org.jetbrains.anko.indeterminateProgressDialog
 import org.jetbrains.anko.selector
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class AddVerseFragment : Fragment() {
 
     companion object {
         fun newInstance() = AddVerseFragment()
+        const val TAG = "MV_AddVerseFragment"
     }
 
     private lateinit var viewModel: AddVerseViewModel
@@ -89,6 +99,59 @@ class AddVerseFragment : Fragment() {
         Log.d("NJW", "$book $chapter:$verse")
         val ref = "$book $chapter:$verse"
         Analytics.trackEvent(Analytics.ADD_VERSE_CLICK, ref)
+        // *** TODO* Check to see if
+
+        // TODO: Dn't let the user add a verse if they already have it in their list
+        // they shludn't have to have a network call for that.
+        makeAddVerseNetworkCall()
+
+    }
+
+    private fun makeAddVerseNetworkCall() {
+        Log.d(TAG, "***** makeAddVerseNetworkCall")
+        if (activity == null || activity?.isFinishing == true) {
+            return
+        }
+        val context = activity as Context
+
+        val dialogFetch = context.indeterminateProgressDialog(message = "Please wait a bit…", title = "Fetching verses")
+
+        val memVersesApi = ServiceGenerator.createPasswordAuthService(MemverseApi::class.java)
+
+        val call = memVersesApi.lookupVerse()
+
+        call.enqueue(object : Callback<VerseResponse> {
+            override fun onResponse(call: Call<VerseResponse>, response: Response<VerseResponse>) {
+                dialogFetch.hide()
+                Log.d(TAG, "makeAddVerseNetworkCall:Response code: " + response.code())
+                if (response.isSuccessful) {
+                    val myVersesResponse = response.body()
+
+                    if (myVersesResponse == null) {
+                        Log.e(TAG, "makeAddVerseNetworkCall network response is null")
+                        Crashlytics.logException(Exception("makeAddVerseNetworkCall Fetch response is null"))
+                    } else {
+                        // TODO: GEt teh verse and do 2nd network call to add it
+                        // TOD: switch to coroutines to avoid callback hell...
+                        // coroutines throw exception...
+                        onVerse
+                    }
+                } else {
+                    Crashlytics.logException(Exception("makeAddVerseNetworkCall network code ${response.code()} error ;url= ${call.request()}; "))
+                    Log.e(TAG, "***makeAddVerseNetworkCallresponse code = ${response.code()}")
+                    onFetchNetworkError()
+                }
+            }
+
+            override fun onFailure(call: Call<VerseResponse>, t: Throwable) {
+                dialogFetch.hide()
+                // ** TODO Use Timber so that crashes in development don't get sent!!!
+                val errorMessage = "***makeAddVerseNetworkCall Call  Failure:${call.request()}${t.message}"
+                Log.e(TAG, errorMessage)
+                Crashlytics.logException(Exception(errorMessage))
+                onFetchNetworkError()
+            }
+        })
 
     }
 
