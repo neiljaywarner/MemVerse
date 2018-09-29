272f8516636a70741266833acf18e4b104d7dc1b
Neil Jay Warner <njwandroid@gmail.com>
2018-09-29 02:36:42 -0500

    work in progress for deep link/dynamic links

--- app/src/main/java/com/spiritflightapps/memverse/ui/AddVerseActivity.kt
+++ app/src/main/java/com/spiritflightapps/memverse/ui/AddVerseActivity.kt
@@ -1,7 +1,10 @@
 package com.spiritflightapps.memverse.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.spiritflightapps.memverse.CurrentUser
 import com.spiritflightapps.memverse.R
 import com.spiritflightapps.memverse.ui.ui.addverse.AddVerseFragment
@@ -16,11 +19,29 @@ class AddVerseActivity : AppCompatActivity() {
             startActivity<LoginActivity>()
             return
         }
+
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+
+        Log.d("NJW", "intent action = $action")
+        Log.d("NJW", "intent uri = $data")
+
+        // adb shell am start
+        //        -W -a android.intent.action.VIEW
+        //        -d "example://gizmos" com.example.android
+
+        // handle deeplink sfa://memverse/add?book=Colossians&chapter=1&verse=17&translation=NIV
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, AddVerseFragment.newInstance())
                     .commitNow()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        //dosoething
+        Log.d("NJW", "dostf")
     }
 
 
